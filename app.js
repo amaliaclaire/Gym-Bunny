f69fb097071e217473ac013aaaf130cd8285b278
Amalia Claire Fergin <acfergin@msn.com>
2018-04-03 15:31:27 -0700

    added connection.js

--- app.js
+++ app.js
@@ -2,11 +2,10 @@ var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
-var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
-var users = require('./routes/users');
+var gym_bunny = require('./routes/gym_bunny');
 
 var app = express();
 
@@ -19,11 +18,10 @@ app.set('view engine', 'hbs');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/users', users);
+app.use('/gym_bunny', gym_bunny);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
